efb73c299e8025a822ef6b4f791f98cb542e143a
antoniomalheirs <antoniomalhe@gmail.com>
2023-08-04 02:12:25 -0300

    Funcionalidades implementadas

--- app/src/main/java/com/example/midia_control/MainActivity.java
+++ app/src/main/java/com/example/midia_control/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.midia_control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -26,7 +27,27 @@ public class MainActivity extends AppCompatActivity
         pause = findViewById(R.id.pause);
         stop = findViewById(R.id.stop);
         prog = findViewById(R.id.seekBar);
+
+        manager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        Barravolume();
     }
+
+    MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener()
+    {
+        @Override
+        public void onCompletion(MediaPlayer player)
+        {
+            Liberarec();
+        }
+    };
+
+    @Override
+    protected void onStop()
+    {
+        Liberarec();
+        super.onStop();
+    }
+
     public void onClick(View v)
     {
         if (v.getId() == R.id.play)
@@ -54,5 +75,55 @@ public class MainActivity extends AppCompatActivity
 
             player = MediaPlayer.create(getApplicationContext(),R.raw.american);
         }
+    }
+
+    private void Liberarec()
+    {
+        if (player != null)
+        {
+            player.stop();
+            player.release();
+        }
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (player != null && player.isPlaying())
+        {
+            player.stop();
+            player.release();
+        }
+    }
+
+    protected void Barravolume()
+    {
+        int volumeMax = manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        int volumeAt = manager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        manager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        prog.setMax(volumeMax);
+        prog.setProgress(volumeAt);
+
+        prog.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                manager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+            }
+        });
     }
 }
